e9f0864aac5cc003bf811fdd64095995a231df97
dev-omafrank <sayhellotodevops@gmail.com>
2026-03-31 01:48:58 +0200

    updated dashboard page

--- resources/views/components/mobile-toggle-button.blade.php
+++ resources/views/components/mobile-toggle-button.blade.php
@@ -1,6 +1,5 @@
 <button
-    @click="sidebarOpen = !sidebarOpen"
-    class="md:hidden fixed top-4 left-4 z-50 w-10 h-10 rounded bg-primary text-white flex items-center justify-center shadow-lg">
+    button @click="sidebarOpen = !sidebarOpen" class="fixed top-4 left-4 z-50 w-10 h-10 rounded bg-primary text-white flex items-center justify-center shadow-lg" style="margin-top: 50px;">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round"
