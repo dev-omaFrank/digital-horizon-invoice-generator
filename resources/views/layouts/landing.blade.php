094168b30ba398e54803d3d37841e6b005c6970d
dev-omafrank <sayhellotodevops@gmail.com>
2026-04-01 05:19:37 +0200

    updated og metadata, and added google analytics

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -3,12 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Ledgerly Invoices - Simple Invoice Generator')</title>
+    <title>@yield('title', 'Ledgerly - Simple Invoice Generator')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-    {{-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> --}}
-    {{-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"> --}}
+    <meta property="og:title" content="Ledgerly">
+    <meta property="og:type" content="Solving the chaos, inefficiency, and lost revenue around invoicing for freelancers, small businesses, and service providers">
+    <meta property="og:image" content="https://iili.io/BKzT8OB.png">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3K0N15VKRE"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-3K0N15VKRE');
+    </script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
